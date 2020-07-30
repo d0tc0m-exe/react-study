3ac2098711aa8abe010aac9842b8a63e78699d3e
xKsIdcFS <54895767+d0tc0m-exe@users.noreply.github.com>
2020-07-30 19:22:23 +0300

    copy in profileReducer

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,28 +1,37 @@
-var ADD_POST = 'ADD-POST';
-var UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
     postData: [
         {id: 1, message: 'Hello, this is post 1'}, 
         {id: 2, message: 'Hello, this is post 2'},
         {id: 3, message: 'Hello, this is post 3'}, 
-        {id: 4, message: 'Hello, this is post 4'},],
+        {id: 4, message: 'Hello, this is post 4'}
+    ],
     newTextData: 'example'
 };
 
 const profileReducer = (state = initialState, action) => {
+
     switch(action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
-                id: 3,
+                id: 5,
                 message: state.newTextData,
             }
-            state.postData.push(newPost);
-            state.newTextData = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newTextData = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.postData = [...state.postData];
+            stateCopy.postData.push(newPost);
+            stateCopy.newTextData = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            
+            let stateCopy = {...state};
+            stateCopy.postData = [...state.postData];
+            stateCopy.newTextData = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
