4da424069d992585f7fff70c57f420bc5fab3c75
Ivan Diakov <vey.the.user@gmail.com>
2020-11-27 23:08:35 +0200

    componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,7 +6,9 @@ import userImage from '../../static/images/user.png';
 class Users extends React.Component {
     constructor(props) {
         super(props);
+    }
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users/").then(response => {
             props.setUsers(response.data.items);
         });
